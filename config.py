df9975fb4fb361085dc940ec4ceaf9614884ca8d
devprorolex <139581378+devprorolex@users.noreply.github.com>
2023-08-11 15:51:11 +0530

    config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5010073453").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/devprorolex/Mr_Rolexx")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/devprorolex/NovaXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
